e2884878a805b3a440423dba6d9bc358e1d02adb
Reddipalli <899167@cognizant.com>
2021-04-28 20:06:11 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(DBEntities db = new DBEntities())
+            {
+                student student = new student
+                {
+                    name = "sumanth",
+                    branch = "cse"
+                };
+                db.students.Add(student);
+                db.SaveChanges();
+            }
+        }
+    }
+}
